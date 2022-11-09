c36600d1f8e17bc9eb000f0596bc44aff8ec0442
Daniel James Szelogowski <dszelogowski@gmail.com>
2022-11-09 16:31:50 -0600

    Update

--- CD-ROM/CD-ROM/Program.py
+++ CD-ROM/CD-ROM/Program.py
@@ -5,6 +5,10 @@ clr.AddReference('System.Drawing')
 from System.Windows.Forms import Application
 import MainForm
 
-Application.EnableVisualStyles()
-form = MainForm.MainForm()
-Application.Run(form)
+try:
+	Application.EnableVisualStyles()
+	form = MainForm.MainForm()
+	Application.Run(form)
+except Exception as e:
+	print("Error:", e)
+	input("Press enter to continue...")	
